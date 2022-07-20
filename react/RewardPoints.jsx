ccdbebe3b9ac93a6c4849f25c59e8817efad4c98
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2022-07-20 00:37:14 -0300

    Adjust search bar size and add reward points counter

---
+++ react/RewardPoints.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const RewardPoints = () => {
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      const response = await axios.get("/api/sessions?items=*");
+      const email = response.data.namespaces.profile.email.value;
+      console.log("aaaa", email);
+      const userData = await axios.get(
+        "https://hc3-node-api.herokuapp.com:43524/rewards",
+        {
+          data: JSON.stringify({ email: "yallatea06@protonmail.com" }),
+        }
+      );
+      console.log("teste", userData.data);
+    };
+
+    getCurrentUser();
+  }, []);
+
+  return <p>{points} pontos</p>;
+};
+
+export default RewardPoints;
